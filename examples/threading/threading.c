f6aba28883fcc77c069336127578c485f63882de
katzkid <kartik.viswanathan@gmail.com>
2026-01-03 20:49:57 +0100

    assignment 4

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,11 +13,31 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    //wait to obtain the mutex
+    usleep( thread_func_args->wait_to_obtain_ms * 1000 );
+    pthread_mutex_lock( thread_func_args->mutex );
+    thread_func_args->thread_complete_success = true; //indicate success after obtaining the mutex
+    //wait to release the mutex
+    usleep( thread_func_args->wait_to_release_ms * 1000 );
+    pthread_mutex_unlock( thread_func_args->mutex );
     return thread_param;
 }
 
-
+/**
+* Start a thread which sleeps @param wait_to_obtain_ms number of milliseconds, then obtains the
+* mutex in @param mutex, then holds for @param wait_to_release_ms milliseconds, then releases.
+* The start_thread_obtaining_mutex function should only start the thread and should not block
+* for the thread to complete.
+* The start_thread_obtaining_mutex function should use dynamic memory allocation for thread_data
+* structure passed into the thread.  The number of threads active should be limited only by the
+* amount of available memory.
+* The thread started should return a pointer to the thread_data structure when it exits, which can be used
+* to free memory as well as to check thread_complete_success for successful exit.
+* If a thread was started succesfully @param thread should be filled with the pthread_create thread ID
+* coresponding to the thread which was started.
+* @return true if the thread could be started, false if a failure occurred.
+*/
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
@@ -28,6 +48,64 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* t_data = malloc( sizeof(struct thread_data) );
+    if( t_data == NULL ) {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+
+    //populate the thread data structure
+    t_data->mutex = mutex;
+    t_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    t_data->wait_to_release_ms = wait_to_release_ms;
+    t_data->thread_complete_success = false; //initialize to false
+    
+    //create the thread
+    int ret = pthread_create( thread, NULL, threadfunc, (void*) t_data );
+    if( ret != 0 ) {
+        ERROR_LOG("Failed to create thread");
+        
+        return false;
+    }
+    DEBUG_LOG("Thread created successfully");
+    return true; //success
+}
+
+int main()
+{
+    pthread_t thread;
+    pthread_mutex_t mutex;
+    int ret;
+    void* t_data_pointer = NULL;
+
+    ret = pthread_mutex_init(&mutex, NULL);
+    if( ret != 0 ) {
+        ERROR_LOG("Failed to initialize mutex");
+        return 1;
+    }
+
+    ret = start_thread_obtaining_mutex( &thread, &mutex, 1000, 1000 );
+    if( ret == false ) {
+            ERROR_LOG("Failed to start thread");
+            return 1;
+        }
+    
+        // Wait for thread to exit
+    ret = pthread_join( thread, &t_data_pointer);
+    
+    if( t_data_pointer != NULL ) {
+        struct thread_data* results = (struct thread_data*)t_data_pointer;
+        if( results->thread_complete_success ) {
+            DEBUG_LOG("Thread completed successfully");
+        } else {
+            ERROR_LOG("Thread did not complete successfully");
+        }
+        // Memory was malloc'd in start_thread_...; MUST free here.
+        free( t_data_pointer );
+    }
+    
+    pthread_mutex_destroy( &mutex );
+
+    return 0;
 }
 
