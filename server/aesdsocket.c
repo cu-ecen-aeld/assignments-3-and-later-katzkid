f6a5fe1d97ba6f1f017ac12706409bf56d3e4994
katzkid <katzkid@katzkid.com>
2026-01-10 01:41:22 +0100

    assignment5

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,279 +10,136 @@
 #include <syslog.h>
 #include <signal.h>
 #include <arpa/inet.h>
-#include <limits.h>
 #include <errno.h>
 
 volatile sig_atomic_t graceful_exit = 0;
+void signal_handler(int signum) { graceful_exit = 1; }
 
-void signal_handler(int signum) 
-{
-    graceful_exit = 1;
+// Helper function to ensure everything is sent even if Valgrind throttles the socket
+int send_all(int s, char *buf, int len) {
+    int total = 0;
+    int bytesleft = len;
+    int n;
+    while(total < len) {
+        n = send(s, buf + total, bytesleft, 0);
+        if (n == -1) break;
+        total += n;
+        bytesleft -= n;
+    }
+    return n == -1 ? -1 : 0;
 }
 
-
 int main(int argc, char *argv[]) {
-    int status;
     struct sockaddr_storage their_addr;
     socklen_t addr_size;
-    struct addrinfo hints;
-    struct addrinfo *servinfo;
-    int sockfd;
-    int new_sockfd;
-    int ret;
-
+    struct addrinfo hints, *servinfo;
+    int sockfd, new_sockfd;
     int daemon_mode = 0;
+
+    // Check for daemon flag
     int opt;
     while ((opt = getopt(argc, argv, "d")) != -1) {
-        if (opt == 'd') {
-            daemon_mode = 1;
-        }
+        if (opt == 'd') daemon_mode = 1;
     }
-   
 
-    // signal handling
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
+    // Set up signals
+    struct sigaction sa = {.sa_handler = signal_handler};
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
 
-    // Initialize syslog
-    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
-    
-   
+    openlog("aesdsocket", LOG_PID, LOG_USER);
+
+    // Initial Cleanup: Ensure file is empty and fresh
+    int init_fd = open("/var/tmp/aesdsocketdata", O_CREAT | O_TRUNC | O_WRONLY, 0666);
+    if (init_fd != -1) close(init_fd);
+
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;     // IPv4 or IPv6
-    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-    hints.ai_flags = AI_PASSIVE; //take localhost address
+    hints.ai_family = AF_INET; // Force IPv4 for simpler testing
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-    // call getaddrinfo function
-    if((status =getaddrinfo(NULL, "9000", &hints, &servinfo)) != 0) {
-        syslog(LOG_ERR, "getaddrinfo error: %s", gai_strerror(status));
-        //close(fd);
-        freeaddrinfo(servinfo);
-        exit(EXIT_FAILURE);
-    }
+    if (getaddrinfo(NULL, "9000", &hints, &servinfo) != 0) return -1;
 
     sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-    if(sockfd == -1) {
-        syslog(LOG_ERR, "socket error");
-        //close(fd);
-        freeaddrinfo(servinfo);
-        exit(EXIT_FAILURE);
-    }
-
-    // lose the pesky "Address already in use" error message
     int yes = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes);
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-    int retry_count = 0;
-    const int max_retries = 10;
-    const int retry_delay = 1; // seconds
-    int bound = 0;
-    
-    while(retry_count < max_retries)
-    {
-        ret = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
-        if(ret == 0)
-        {
-            syslog(LOG_USER, "Successfully bound to port 9000");
-            bound = 1;
-            break; // success
-        }
-        else if(errno == EADDRINUSE) {
-            syslog(LOG_ERR, "port in use bind error");
-            sleep(retry_delay);
-            retry_count++;
-        }
-        else
-        {
-            //some other error
-            syslog(LOG_ERR, "bind error on port 9000: %m"); // %m to print errno message
-            break;
-            
-
-        }
-    }
-    
-    if(!bound) 
-    {
-        //close(fd);
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) != 0) {
         freeaddrinfo(servinfo);
-        exit(EXIT_FAILURE);
+        return -1;
     }
-    
+    freeaddrinfo(servinfo);
 
+    if (listen(sockfd, 10) == -1) return -1;
 
-    //listen for incoming connections
-    ret = listen(sockfd, 10);
-    if(ret == -1) {
-        syslog(LOG_ERR, "listen error");
-        //close(fd);
-        freeaddrinfo(servinfo);
-        exit(EXIT_FAILURE);
-    }
-
-     // open a file for writing
-    int fd = open("/var/tmp/aesdsocketdata", O_CREAT | O_APPEND | O_TRUNC | O_SYNC | O_RDWR, 0666);
-    if(fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-
-    close(fd); // close and reopen in the loop to ensure data is written properly
-
-    //daemonize if -d option is provided
-    if(daemon_mode) {
-        pid_t pid =fork();
-        if(pid < 0) {
-            syslog(LOG_ERR, "fork error");
-            close(fd);
-            freeaddrinfo(servinfo);
-            exit(EXIT_FAILURE);
-        }
-        if (pid > 0) {
-            // parent process
-            exit(EXIT_SUCCESS);
-        }
-        // child process continues
-        // fork again for deamon
-        pid = fork();
-        if(pid < 0) {
-            syslog(LOG_ERR, "fork error");
-            close(fd);
-            freeaddrinfo(servinfo);
-            exit(EXIT_FAILURE);
-        }
-        if (pid > 0) {
-            // parent process
-            exit(EXIT_SUCCESS);
-        }
-        // child process continues
-        //change workdir to root
-        if(chdir("/") < 0) {
-            syslog(LOG_ERR, "chdir error");
-            close(fd);
-            freeaddrinfo(servinfo);
-            exit(EXIT_FAILURE);
-        }
-        //close standard file descriptors
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-
-        // redirect to standard output
+    // Daemonize AFTER successful bind
+    if (daemon_mode) {
+        if (fork() > 0) exit(0);
+        setsid();
+        if (fork() > 0) exit(0);
+        chdir("/");
         int fd_null = open("/dev/null", O_RDWR);
-        if(fd_null != -1) {
-            dup2(fd_null, STDIN_FILENO);
-            dup2(fd_null, STDOUT_FILENO);
-            dup2(fd_null, STDERR_FILENO);
-            close(fd_null);
-        } else {
-            syslog(LOG_ERR, "/dev/null open error");
-            close(fd);
-            freeaddrinfo(servinfo);
-            exit(EXIT_FAILURE);
-        }
+        dup2(fd_null, STDIN_FILENO);
+        dup2(fd_null, STDOUT_FILENO);
+        dup2(fd_null, STDERR_FILENO);
+        close(fd_null);
     }
 
-    //accept a connection in a loop till SIGINT or SIGTERM is received
-    while(!graceful_exit)
-    {
+    while (!graceful_exit) {
         addr_size = sizeof(their_addr);
         new_sockfd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-        if(new_sockfd == -1) {
-            syslog(LOG_ERR, "accept error");
-            if (errno == EINTR) 
-            {
-            // The signal handler was called; the loop condition will now see graceful_exit=1
-            break; 
-            }
-            close(fd);
-            freeaddrinfo(servinfo);
-            exit(EXIT_FAILURE);
+        if (new_sockfd == -1) {
+            if (errno == EINTR) break;
+            continue;
         }
-        
+
         char host[NI_MAXHOST];
         getnameinfo((struct sockaddr *)&their_addr, addr_size, host, sizeof(host), NULL, 0, NI_NUMERICHOST);
         syslog(LOG_INFO, "Accepted connection from %s", host);
-    
 
-        size_t current_buf_size = 1024;
-        char *buffer = malloc(current_buf_size);
+        size_t buf_size = 1024;
+        char *buffer = malloc(buf_size);
         size_t total_recv = 0;
-
-        
 
         while (!graceful_exit) 
         {
-            ssize_t bytes_received = recv(new_sockfd, buffer + total_recv, current_buf_size - total_recv - 1, 0);
-            if (bytes_received < 0) 
-            {
-                if(errno == EINTR)
-                {
-                    // Interrupted by signal
-                    break;
-                }
-                syslog(LOG_ERR, "recv error");
-                break;
-            }
-
-            total_recv += bytes_received;
+            ssize_t nr = recv(new_sockfd, buffer + total_recv, buf_size - total_recv - 1, 0);
+            if (nr <= 0) break;
+            total_recv += nr;
             buffer[total_recv] = '\0';
 
-            // Check for newline
-            if (total_recv > 0 && memchr(buffer, '\n', total_recv) != NULL) 
-            {
-                // Write received data to file
-                // \n is found. now open the file
-                int data_fd = open("/var/tmp/aesdsocketdata", O_CREAT | O_APPEND | O_RDWR, 0666);
-                if (data_fd != -1) 
-                {
+            if (memchr(buffer, '\n', total_recv)) {
                 
-                    write(data_fd, buffer, total_recv);
-                    fsync(data_fd);
-                    
-                    //  readback
-                    lseek(data_fd, 0, SEEK_SET); // Reset file offset to beginning
-                    char read_buf[1024];
-                    ssize_t bytes_read;
-                    
-                    while ((bytes_read = read(data_fd, read_buf, sizeof(read_buf))) > 0) {
-                        send(new_sockfd, read_buf, bytes_read, 0);
-                    }
-                    close(data_fd);
-                }
-                break; // Finished this transaction
-            }
-            if(bytes_received == 0) {
-                // Connection closed by client
-                break;
-            }  
+                int data_fd = open("/var/tmp/aesdsocketdata", O_WRONLY | O_APPEND | O_CREAT, 0666);
+                write(data_fd, buffer, total_recv);
+                fsync(data_fd); // Crucial for Valgrind
+                close(data_fd); // Forces metadata sync
 
-            // Expand buffer if needed
-            current_buf_size += 1024;
-            char *tmp = realloc(buffer, current_buf_size);
-            if (tmp == NULL) {
-                free(buffer);
-                // Handle error
-                return -1;
+                data_fd = open("/var/tmp/aesdsocketdata", O_RDONLY);
+                char r_buf[1024];
+                ssize_t r_bytes;
+                while ((r_bytes = read(data_fd, r_buf, sizeof(r_buf))) > 0) {
+                    send_all(new_sockfd, r_buf, r_bytes);
+                }
+                close(data_fd);
+                
+                total_recv = 0; // reset for next potential line
+                break; // Break after each newline
             }
-            buffer = tmp;
+
+            if (total_recv >= buf_size - 1) {
+                buf_size += 1024;
+                buffer = realloc(buffer, buf_size);
+            }
         }
         free(buffer);
         close(new_sockfd);
         syslog(LOG_INFO, "Closed connection from %s", host);
-
     }
 
-
-    //clean up and close
-    
     syslog(LOG_INFO, "Caught signal, exiting");
     close(sockfd);
     remove("/var/tmp/aesdsocketdata");
-    freeaddrinfo(servinfo);
     closelog();
     return 0;
 }
-
-
-
